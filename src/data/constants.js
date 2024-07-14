755f6ba735a1617aeb8b05d7862b0e3f0940148f
Urimella Uma sri <48849066+umasriurimella@users.noreply.github.com>
2024-07-14 10:48:26 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,225 @@
+export const Bio = {
+  name: "URIMELLA UMA SRI",
+  roles: [
+    "Front-End Developer"
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/umasriurimella",
+  resume:
+    "https://drive.google.com/file/d/1iNAp1cx1RRRLyvkkr_xEhR4tM5_TTHEW/view",
+  linkedin: "https://www.linkedin.com/in/umasri-urimella",
+  twitter: "https://x.com/UUrimella",
+  
+ 
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      
+    
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Git_icon.svg/2048px-Git_icon.svg.png"
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+     
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://www.skec.ac.in/wp-content/uploads/2015/12/skec-logo1.png",
+    school:"Sreekavitha Engineering College",
+    date: "Aug 2016 - Sep 2020",
+    grade: "7.1 CGPA",
+    desc: "I have completed Bachelor's degree in Computer Science and Engineering at Sreekavitha Engineering College , Karepalli.",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://cdn2.vectorstock.com/i/1000x1000/34/01/college-rubber-stamp-vector-12393401.jpg",
+    school: "Sri Krishnaveni Junior College",
+    date: "June 2014 - Apr 2016",
+    grade: "79.4%",
+    desc: "I have completed my Intermidiate at Sri Krishnaveni Junior College,Tekulapalli",
+    degree: "Intermidiate",
+  },
+  {
+    id: 2,
+    img: "https://www.schoolnearme.in/assets/images/logo/1176246503.jpg",
+    school: "Priyadharshini School",
+    date: "Apr 2014 ",
+    grade: "8.2 CGPA ",
+    desc: "I have completed my class 10 education at Priyadharshini school,Yellandu",
+    degree: "SSC(X)",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Youtube Clone",
+    description:
+      "Youtube-Clone is a React application designed to recreate the functionality and user experience of YouTube. It is made by using HTML,CSS,Javascript and React.In this react project i have used the YouTube data API to get the YouTube video List and real time statistics of the YouTube videos.",
+    image:"Youtube.jpg",
+    tags: [
+      "Html",
+      "CSS",
+      "JavaScript",
+      "React Js",
+      
+    ],
+    category: "React app",
+    github: "https://github.com/umasriurimella/Youtube-Clone",
+    webapp: "https://umasriurimella.github.io/Youtube-Clone/",
+  },
+  {
+    id: 0,
+    title: "Edusity",
+    description:
+      "Edusity is complete responsive website using HTML,CSS,JavaScript and react Js.In this website added a features like react smooth scroll,slider and working contact form.It is college or university website",
+    image:
+      "Edusity.png",
+    tags: ["Html","CSS","Javascript","React Js"],
+    category: "React app",
+    github: "https://github.com/umasriurimella/Edusity",
+    webapp: "https://umasriurimella.github.io/Edusity/",
+  },
+  {
+    id: 1,
+    title: "Gemini clone",
+    description:
+      "Gemini clone is made by using HTML,CSS,JavaScript and ReactJS.It replicates the search interface of Google Gemini.This projects includes API integration with Google Gemini's language model,allowing users to experience a similar search experience. ",
+    image:
+      "Gemini.png",
+    tags: 
+      ["Html","CSS","Javascript","React Js"],
+    
+    category: "React app",
+    github: "https://github.com/umasriurimella/Gemini-Clone",
+    webapp: "https://umasriurimella.github.io/Gemini-Clone/",
+  },
+  
+  {
+    id: 2,
+    title: "News App",
+    description:
+      "News app is developed by using HTML,CSS,Javascript,React Js and Bootstrap.The main focus of this application is to connect news articles from all around the world and deliver it to user as fast as possible in best visualize way.This application was run by using News API it works locally",
+    image:
+      "news-mag.png",
+    tags: ["Html","CSS","Javascript","React Js","Bootstrap"],
+    category: "React app",
+    github: "https://github.com/umasriurimella/News-Mag",
+    webapp: "https://umasriurimella.github.io/News-Mag/",
+  },
+  
+  {
+    id: 3,
+    title: "Quiz App",
+    description:
+      "A quiz app made with Html,CSS,Javascript and React Js. It has a score card we can check the score and reset option also there we can reset the quiz.When we select the option it shows the correct answer and it is correct or not.",
+    image:
+      "Quiz.png",
+    tags:["Html","CSS","Javascript","React Js"],
+    category: "React app",
+    github: "https://github.com/umasriurimella/Quiz-App",
+    webapp: "https://umasriurimella.github.io/Quiz-App/",
+  },
+  {
+  id: 4,
+    title: "whack a mole Game",
+    description:
+      "whack a mole Game is made by using HTML,CSS and Javascript.In this project it is created by using game logic and animation,adding sound effects,and keeping score.",
+    image:
+      "Game.png",
+    tags:["Html","CSS","Javascript"],
+    category: "Javascript project",
+    github: "https://github.com/umasriurimella/whack-a-mole",
+    webapp: "https://umasriurimella.github.io/whack-a-mole/",
+  },
+  {id: 5,
+    title: "Github wrapper",
+    description:
+      "A Github wrapper project is made by using HTML,CSS and Javascript.It is a fully functional GitHub API Wrapper project.In this project i have created Github user profile cards.By using the Fetch API we can access the real Github profiles when we give the github profile details it fetches the information from real Github.",
+    image:
+      "Github.png",
+    tags:["Html","CSS","Javascript"],
+    category: "Javascript project",
+    github: "https://github.com/umasriurimella/Github-wrapper",
+    webapp: "https://umasriurimella.github.io/Github-wrapper/",
+  },
+  {id: 6,
+    title: "Notes App",
+    description:
+      "A Notes App project is made by using HTML,CSS and Javascript.It consists of features like add note and delete note.By using these features we can add and delete the notes.",
+    image:
+      "Notes.png",
+    tags:["Html","CSS","Javascript",],
+    category: "Javascript project",
+    github: "https://github.com/umasriurimella/Notes-App",
+    webapp: "https://umasriurimella.github.io/Notes-App/",
+  },
+  {
+    id: 7,
+    title: "Counter App",
+    description:
+      "Counter is small Javascript project it is made by using HTML,CSS and Javascrit.It is counter project consists of increment,decrement and set Zero features.",
+    image:
+      "Counter.png",
+    tags:["Html","CSS","Javascript"],
+    category: "Javascript project",
+    github: "https://github.com/umasriurimella/Counter-App",
+    webapp: "https://umasriurimella.github.io/Counter-App/",
+  },
+
+];
+
